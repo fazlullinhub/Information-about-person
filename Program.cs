b68908ff3e709e73a8badccdf2a419a9c74bae22
fazlullinhub <164046064+fazlullinhub@users.noreply.github.com>
2024-05-24 12:53:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+namespace ConsoleApplication5
+{
+    public partical class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var person = textBox1.Text + " " + textBox2.Text;
+            MessageBox.Show(person, "Your personal data", MessageBoxButtons.YesNo);
+        }
+    }
+}
+
